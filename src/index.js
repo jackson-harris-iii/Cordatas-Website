9da0eebbe4b794f5d40b104e380cf7aaf54b6b09
Jackson <Jackson.harris3@gmail.com>
2020-03-12 15:22:39 -0700

    update landing page

--- src/index.js
+++ src/index.js
@@ -22,8 +22,9 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
+import "assets/css/cordatas-styles.scss";
 
-import Index from "views/Index.js";
+import Index from "views/Index.jsx";
 import Landing from "views/examples/Landing.js";
 import Login from "views/examples/Login.js";
 import Profile from "views/examples/Profile.js";
