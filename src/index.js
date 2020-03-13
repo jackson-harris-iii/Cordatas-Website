b3289078abe5e6fd0304dc1569b16d80408babb2
Jackson <Jackson.harris3@gmail.com>
2020-03-12 19:29:30 -0700

    build

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
-import "assets/css/cordatas-styles.scss";
+import "assets/scss/cordatas-styles.scss";
 
 import Index from "views/Index.jsx";
 import Landing from "views/examples/Landing.js";
